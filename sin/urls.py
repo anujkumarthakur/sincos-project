cd56a80e648abfddeba86bdd774faae96d1d8622
anujkumarthakur <anuj85500@gmail.com>
2019-09-25 05:51:40 +0530

    add product templates

--- sin/urls.py
+++ sin/urls.py
@@ -9,5 +9,18 @@ urlpatterns = [
         path('about/',views.about,name='about'),
         path('services/',views.services,name='services'),
         path('contact/',views.contact,name='contact'),
-        path('productdetail/',views.productdetail,name='productdetails')
+        path('productdetail/',views.productdetail,name='productdetails'),
+        path('machine1',views.machine1,name='machine1'),
+        path('machine2',views.machine2,name='machine2'),
+        path('machine3',views.machine3,name='machine3'),
+        path('machine4',views.machine4,name='machine4'),
+        path('machine5',views.machine5,name='machine5'),
+        path('machine6',views.machine6,name='machine6'),
+        path('machine7',views.machine7,name='machine7'),
+        path('machine8',views.machine8,name='machine8'),
+        path('machine9',views.machine9,name='machine9'),
+        path('machine10',views.machine10,name='machine10'),
+        path('machine11',views.machine11,name='machine11'),
+        path('machine12',views.machine12,name='machine12')
+        
         ]
--- sin/views.py
+++ sin/views.py
@@ -28,3 +28,48 @@ def productdetail(request):
     return render(request,'productitemsdetails.html')
 
 # Create your views here.
+
+#All machines links here
+
+def machine1(request):
+    return render(request,'machine1.html')
+
+def machine2(request):
+    return render(request,'machine2.html')
+
+def machine3(request):
+    return render(request,'machine3.html')
+
+
+
+def machine4(request):
+    return render(request,'machine4.html')
+
+def machine5(request):
+    return render(request,'machine5.html')
+
+def machine6(request):
+    return render(request,'machine6.html')
+
+def machine7(request):
+    return render(request,'machine7.html')
+
+def machine8(request):
+    return render(request,'machine8.html')
+
+def machine9(request):
+    return render(request,'machine9.html')
+
+def machine10(request):
+    return render(request,'machine10.html')
+
+def machine11(request):
+    return render(request,'machine11.html')
+
+def machine12(request):
+    return render(request,'machine12.html')
+
+
+
+
+
